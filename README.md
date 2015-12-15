8c5c4f6c8365cb87bdb6255dcf87397780c0e49b
Martin Lindenberg <martin.lindenberg@home24.de>
2015-12-15 16:13:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+Serverless Plugin ALERTING
+==========================
+
+This Plugin adds Cloudwatch Alarms with SMS notifications for your Lambda functions.
+
+* make sure that aws and serverless are installed
+* install this plugin to your projects plugins folder (projectfolder/plugins/serverless-plugin-alerting)
+* add the plugin to your s-project.json file
+```
+"plugins": [
+    {
+      "path": "serverless-plugin-alerting"
+    }
+]
+```
+* place the alerting.json file next to your s-function.json file right in the functions folder
+Example file:
+```
+{
+    "notificationTopicStageMapping": {
+        "development": "your-dev-sns-topic",
+        "testing": "your-testing-sns-topic",
+        "staging": "your-staging-sns-topic",
+        "live": "your-live-sns-topic"
+    },
+    "alerts": {
+        "Duration": {
+            "enabled": true,
+            "alarmNamespace": "AWS/Lambda",
+            "description": "Alarm if duration of the importer is above 500ms",
+            "alarmStatisticType": "Maximum",
+            "alarmPeriod": "60",
+            "alarmThreshold": "500",
+            "comparisonOperator": "GreaterThanOrEqualToThreshold",
+            "evaluationPeriod": "1"
+        },
+        "Errors": {
+            "enabled": true,
+            "alarmNamespace": "AWS/Lambda",
+            "description": "Alarm if function returns an error",
+            "alarmStatisticType": "Sum",
+            "alarmPeriod": "60",
+            "alarmThreshold": "1",
+            "comparisonOperator": "GreaterThanOrEqualToThreshold",
+            "evaluationPeriod": "1"
+        },
+        "Throttles": {
+            "enabled": true,
+            "alarmNamespace": "AWS/Lambda",
+            "description": "Alarm if function has more than 5 throttled requests",
+            "alarmStatisticType": "Sum",
+            "alarmPeriod": "60",
+            "alarmThreshold": "5",
+            "comparisonOperator": "GreaterThanOrEqualToThreshold",
+            "evaluationPeriod": "1"
+        }
+    }
+}
+
+```
+* Notification-Topics:
+** Here you can set a SNS Topic that receives Messages, if a metric triggers an alarm.
+** you can attach another lambda function to the SNS-Topic to do anything on these alarms.
+For example Push a notification to a messaging system like slack, send a email or push data to any Rest-Api.
---
+++ index.js
@@ -0,0 +1,126 @@
+'use strict';
+
+module.exports = function(serverlessPath) {
+
+    const path     = require('path'),
+        fs         = require('fs'),
+        SPlugin    = require(path.join(serverlessPath, 'ServerlessPlugin')),// You can access modules in Serverless by following this convention.
+        BbPromise  = require('bluebird'); // Serverles uses Bluebird Promises and we recommend you do to because they are super helpful :)
+
+    class ServerlessPluginAlerting extends SPlugin {
+        constructor(S, config) {
+            super(S, config);
+        }
+
+        static getName() {
+            return 'com.serverless.' + ServerlessPluginAlerting.name;
+        }
+
+        registerHooks() {
+            this.S.addHook(this._addAlertsAfterDeploy.bind(this), {
+                action: 'functionDeploy',
+                event:  'post'
+            });
+
+            return Promise.resolve();
+        }
+
+        _addAlertsAfterDeploy(evt) {
+            let _this = this;
+
+            return new BbPromise(function (resolve, reject) {
+
+                if (_this.S.cli.context != 'function' || _this.S.cli.contextAction != 'deploy') {
+                    return;
+                }
+
+                // candidate for function
+                for (var i in evt.functions) {
+                    var fn = evt.functions[i];
+                    var alertPathFile = fn.pathFunction.replace('s-function.json', 'alerting.json');
+
+                    if (!fs.existsSync(alertPathFile)) {
+                        continue;
+                    }
+
+                    try {
+                        var alertContent = JSON.parse(fs.readFileSync(alertPathFile));
+                    } catch (e) {
+                        console.log('alerting.json not readable');
+                        continue;
+                    }
+
+                    var functionName = _this._getFunctionNameByArn(fn.deployedAliasArn, fn.deployedAlias);
+                    console.log('SERVERLESS-PLUGIN-ALERTING: adding alerts to ' + functionName);
+
+                    _this._setNotificationActionByArn(
+                        fn.deployedAliasArn,
+                        alertContent.notificationTopicStageMapping,
+                        fn.deployedAlias
+                    );
+
+                    for (var metricname in alertContent.alerts) {
+                        exec(_this._getConfigAlarm(functionName, metricname, alertContent.alerts[metricname]), function () {
+                            console.log('alarm added');
+                        });
+                    }
+                }
+
+                return resolve(evt);
+            });
+        }
+
+        _getFunctionNameByArn(arn, stage) {
+            var name = arn.split(':function:');
+            return name[1].replace(':' + stage, '');
+        }
+
+        _setNotificationActionByArn(arn, map, stage) {
+            let _this = this;
+
+            var name = arn.split(':function:');
+            name = name[0].replace(':lambda:', ':sns:');
+            _this._notificationAction = name + ':' + map[stage];
+        }
+
+        _getConfigAlarm(functionName, metric, alertConfig) {
+            let _this = this;
+
+            return _this._getPutMetricAlarmCmd({
+                'alarmName': functionName + ' ' + metric,
+                'alarmDescription': alertConfig.description,
+                'metricName': metric,
+                'alarmNamespace': alertConfig.alarmNamespace,
+                'alarmStatisticType': alertConfig.alarmStatisticType,
+                'alarmPeriod': alertConfig.alarmPeriod,
+                'alarmThreshold': alertConfig.alarmThreshold,
+                'comparisonOperator': alertConfig.comparisonOperator,
+                'functionName': functionName,
+                'evaluationPeriod': alertConfig.evaluationPeriod,
+                'notificationAction': _this._notificationAction
+            });
+        }
+
+        _getPutMetricAlarmCmd(config) {
+            var addCommand = 'aws cloudwatch put-metric-alarm ';
+            addCommand += ' --alarm-name "' + config.alarmName + '" ';
+            addCommand += ' --alarm-description "' + config.alarmDescription + '" ';
+            addCommand += ' --metric-name ' + config.metricName + ' ';
+            addCommand += ' --namespace "' + config.alarmNamespace + '" ';
+            addCommand += ' --statistic ' + config.alarmStatisticType + ' ';
+            addCommand += ' --period ' + config.alarmPeriod + ' ';
+            addCommand += ' --threshold ' + config.alarmThreshold + ' ';
+            addCommand += ' --comparison-operator ' + config.comparisonOperator + ' ';
+            addCommand += ' --dimensions  Name=FunctionName,Value=' + config.functionName + ' ';
+            addCommand += ' --evaluation-periods ' + config.evaluationPeriod + ' ';
+            addCommand += ' --alarm-actions ' + config.notificationAction + ' ';
+            addCommand += ' --ok-actions ' + config.notificationAction + ' ';
+            addCommand += ' --insufficient-data-actions ' + config.notificationAction + ' ';
+
+            return addCommand;
+        };
+    }
+
+    return ServerlessPluginAlerting;
+
+};
